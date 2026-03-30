24885faeddefe937e1b261c4cd4a9d08cb5f1b5e
potatostore <a0105775135976@gmail.com>
2026-03-30 09:43:50 +0900

    .

--- src/level1/Member.java
+++ src/level1/Member.java
@@ -1,13 +1,15 @@
 package level1;
 
-public class Member {
-
-    // TODO: 캡슐화에 적절한 접근 제어자를 사용하세요.
-    String name;
-    boolean assignmentSubmitted;
+public abstract class Member {
+    private String name;
+    private boolean assignmentSubmitted;
 
     public Member(String name) {
         // TODO: name이 null이거나, 빈 문자열이거나, 공백만 있는 문자열이면 IllegalArgumentException 예외를 발생시키세요.
+        String compName = name.replace(" ", "");
+        if(compName == null || compName.isEmpty()){
+            throw new IllegalArgumentException("이름은 비어 있을 수 없습니다.");
+        }
 
         this.name = name;
         this.assignmentSubmitted = false;
@@ -17,7 +19,15 @@ public class Member {
         return name;
     }
 
-    public boolean isAssignmentSubmitted() {
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setAssignmentSubmitted(boolean assignmentSubmitted) {
+        this.assignmentSubmitted = assignmentSubmitted;
+    }
+
+    public boolean getAssignmentSubmitted() {
         return assignmentSubmitted;
     }
 
@@ -26,5 +36,5 @@ public class Member {
     }
 
     // TODO: getRole(), doTask()를 추상 메서드로 선언하세요.
-
+    public abstract
 }
