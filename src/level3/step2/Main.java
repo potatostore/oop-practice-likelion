62a1376c43d9c95b346a9daad2cd8f10d4485bee
potatostore <a0105775135976@gmail.com>
2026-03-31 13:46:14 +0900

    .

--- src/level3/step2/Main.java
+++ src/level3/step2/Main.java
@@ -9,7 +9,7 @@ public class Main {
         Member[] members = new Member[3];
         members[0] = new BabyLion("김씨");
         members[1] = new Leader("이씨");
-        members[2] = new Staff("박씨");
+        members[2] = new Staff("최씨");
 
         // TODO: ClubManager 객체를 생성 후 해당 객체의 메서드를 활용하여 예시 코드 완성하기
         ClubManager manager = new ClubManager();
