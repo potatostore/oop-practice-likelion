57e03e668e2d633a3e24f667170d1f209a8bf56d
potatostore <a0105775135976@gmail.com>
2026-03-31 13:30:02 +0900

    .

--- src/level3/step2/ClubManager.java
+++ src/level3/step2/ClubManager.java
@@ -1,11 +1,17 @@
 package level3.step2;
+
+import level1.Member;
+import level2.*;
+
 public class ClubManager {
 
     public void startActivities(Member[] members) {
         for (Member member : members) {
             // TODO: "<이름> / 역할: <역할>" 형식으로 출력하세요.
+            System.out.println(member.getName() + " / 역할 : " + member.getRole());
 
             // TODO: 타입을 구분하지 말고, 공통 메서드 doTask()를 호출하세요.
+            member.doTask();
 
             System.out.println("-------------------");
         }
@@ -15,6 +21,9 @@ public class ClubManager {
         for (Member member : members) {
             // TODO: BabyLion 타입인 경우에만 submitAssignment()를 호출하세요.
             // 힌트: 이번 단계의 핵심 흐름은 다형성으로 처리하되, 꼭 필요한 경우에만 instanceof를 제한적으로 사용합니다.
+            if(member instanceof BabyLion){
+                member.submitAssignment();
+            }
         }
     }
 }
--- src/level3/step2/Main.java
+++ src/level3/step2/Main.java
@@ -1,9 +1,20 @@
 package level3.step2;
 
+import level1.Member;
+import level2.*;
+
 public class Main {
     public static void main(String[] args) {
         // TODO: Member 타입 배열 members를 선언하고, BabyLion, Staff, Leader 객체를 담아보세요.
+        Member[] members = new Member[3];
+        members[0] = new BabyLion("김씨");
+        members[1] = new Leader("이씨");
+        members[2] = new Staff("박씨");
 
         // TODO: ClubManager 객체를 생성 후 해당 객체의 메서드를 활용하여 예시 코드 완성하기
+        ClubManager manager = new ClubManager();
+
+        manager.startActivities(members);
+        manager.submitAssignments(members);
     }
 }
